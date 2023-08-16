8967395b1a40197bef800c01878457b39f7844c5
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-08-16 17:07:34 +0300

    Update ch.py

--- ch.py
+++ ch.py
@@ -1,6 +1,6 @@
 import telebot
+import requests
 from pydub import AudioSegment
-from pydub.playback import play
 from io import BytesIO
 
 # استبدال "TOKEN" بتوكن البوت الخاص بك
@@ -17,19 +17,27 @@ def convert_to_audio(message):
     file_info = bot.get_file(file_id)
     file_path = file_info.file_path
     
-    video_path = f"https://api.telegram.org/file/botTOKEN/{file_path}"
+    video_url = f"https://api.telegram.org/file/botTOKEN/{file_path}"
     
-    # تحويل الفيديو إلى ملف صوتي
-    video = AudioSegment.from_file(video_path, format="webm")
-    audio = video.set_channels(1).set_frame_rate(16000)
+    # تحميل ملف الفيديو
+    response = requests.get(video_url)
     
-    # تحويل الصوت إلى بيانات قابلة للإرسال
-    audio_io = BytesIO()
-    audio.export(audio_io, format="ogg")
-    audio_io.seek(0)
-    
-    # إرسال الرسالة الصوتية إلى المستخدم
-    bot.send_voice(chat_id, audio_io, reply_to_message_id=message.message_id)
+    if response.status_code == 200:
+        video_data = response.content
+        
+        # تحويل الفيديو إلى ملف صوتي
+        video = AudioSegment.from_file(BytesIO(video_data), format="webm")
+        audio = video.set_channels(1).set_frame_rate(16000)
+        
+        # تحويل الصوت إلى بيانات قابلة للإرسال
+        audio_io = BytesIO()
+        audio.export(audio_io, format="ogg")
+        audio_io.seek(0)
+        
+        # إرسال الرسالة الصوتية إلى المستخدم
+        bot.send_voice(chat_id, audio_io, reply_to_message_id=message.message_id)
+    else:
+        bot.send_message(chat_id, "حدث خطأ أثناء تحميل الفيديو.")
 
 # بدء استماع البوت
 bot.polling()
