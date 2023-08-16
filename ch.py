063a43e230617b38a4426098ccaaa16445d8aaff
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-08-16 15:03:05 +0300

    Update ch.py

--- ch.py
+++ ch.py
@@ -1,58 +1,37 @@
 import telebot
-from telebot import types
 import os
+from moviepy.editor import VideoFileClip
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot = telebot.TeleBot('6104906824:AAFfdgn6fUd8DcDMOMkTNZavHYRKAGSSx8g')
 
-# Dictionary to store user states (whether they are waiting for a video or not)
-user_states = {}
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    user_id = message.from_user.id
-    user_states[user_id] = 'waiting_for_video'
-    
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    item = types.KeyboardButton("Send Video")
-    markup.add(item)
-    
-    bot.send_message(user_id, "Please send a video:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Welcome to the video to audio bot! Please send a video.")
 
 @bot.message_handler(content_types=['video'])
-def handle_video(message):
-    user_id = message.from_user.id
-    if user_id in user_states and user_states[user_id] == 'waiting_for_video':
-        user_states[user_id] = 'not_waiting'
-        
-        file_info = bot.get_file(message.video.file_id)
-        file_path = file_info.file_path
-        
-        video_path = os.path.join("videos", file_path)
-        voice_path = os.path.join("voices", f"{user_id}_voice.ogg")
-        
-        downloaded_file = bot.download_file(file_path)
-        with open(video_path, 'wb') as f:
-            f.write(downloaded_file)
-        
-        os.system(f"ffmpeg -i {video_path} -vn -acodec libopus {voice_path}")
-        
-        voice_file = open(voice_path, 'rb')
-        bot.send_voice(user_id, voice_file)
-        
-        os.remove(video_path)
-        os.remove(voice_path)
-        
-        bot.send_message(user_id, "Here is the voice from the video.")
-    else:
-        bot.send_message(user_id, "Please start with /start to initiate the process.")
+def convert_to_audio(message):
+    chat_id = message.chat.id
+    file_info = bot.get_file(message.video.file_id)
+    file_path = file_info.file_path
 
-@bot.message_handler(func=lambda message: True)
-def handle_other_messages(message):
-    user_id = message.from_user.id
-    if user_id in user_states and user_states[user_id] == 'waiting_for_video':
-        bot.send_message(user_id, "Please send a video using the provided button.")
-    else:
-        bot.send_message(user_id, "Please start with /start to initiate the process.")
+    downloaded_file = bot.download_file(file_path)
+    video_filename = 'video.mp4'
+
+    with open(video_filename, 'wb') as video_file:
+        video_file.write(downloaded_file)
+
+    audio_filename = 'audio.wav'
+    video_clip = VideoFileClip(video_filename)
+    video_clip.audio.write_audiofile(audio_filename)
+
+    with open(audio_filename, 'rb') as audio_file:
+        bot.send_audio(chat_id, audio_file)
+
+    # Clean up temporary files
+    os.remove(video_filename)
+    os.remove(audio_filename)
+
+    bot.send_message(chat_id, "Here is the audio extracted from the video.")
 
 bot.polling()
