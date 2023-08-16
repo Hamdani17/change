650ba7978e29345ff0ab900daabf6a7ef05e49c3
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-08-16 16:34:11 +0300

    Update ch.py

--- ch.py
+++ ch.py
@@ -1,41 +1,49 @@
 import telebot
 import os
 from pydub import AudioSegment
+from pydub.playback import play
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot = telebot.TeleBot('6104906824:AAFfdgn6fUd8DcDMOMkTNZavHYRKAGSSx8g')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hello! Send me a video, and I'll extract and send back the audio.")
-
-@bot.message_handler(content_types=['video'])
-def handle_video(message):
+@bot.message_handler(content_types=['audio'])
+def handle_audio(message):
     try:
-        # Download the video
-        file_info = bot.get_file(message.video.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        video_path = 'input_video.mp4'
-        with open(video_path, 'wb') as new_file:
-            new_file.write(downloaded_file)
+        # Download the audio file
+        audio_info = bot.get_file(message.audio.file_id)
+        audio_file = bot.download_file(audio_info.file_path)
 
-        # Extract audio from the video
-        audio_path = 'extracted_audio.wav'
-        video = AudioSegment.from_file(video_path)
-        audio = video.export(audio_path, format='wav')
+        # Save the downloaded audio to a file
+        audio_path = 'original_audio.ogg'
+        with open(audio_path, 'wb') as f:
+            f.write(audio_file)
 
-        # Send the extracted audio back
-        bot.send_audio(message.chat.id, audio, title='Extracted Audio')
+        # Load the original audio
+        original_audio = AudioSegment.from_file(audio_path, format='ogg')
 
-        # Clean up the temporary files
-        os.remove(video_path)
+        # Simulate "recording" by playing the audio
+        play(original_audio)
+
+        # Save the "recorded" audio to a file
+        recorded_audio_path = 'recorded_audio.ogg'
+        with open(recorded_audio_path, 'wb') as f:
+            original_audio.export(f, format='ogg')
+
+        # Send the "recorded" audio back to the user
+        with open(recorded_audio_path, 'rb') as f:
+            bot.send_audio(message.chat.id, f)
+
+        # Clean up temporary files
         os.remove(audio_path)
+        os.remove(recorded_audio_path)
+
     except Exception as e:
-        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+        print(e)
+        bot.send_message(message.chat.id, "An error occurred.")
 
 @bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.reply_to(message, "Send me a video to extract audio!")
+def handle_other_messages(message):
+    bot.send_message(message.chat.id, "Please send an audio file.")
 
 # Start the bot
 bot.polling()
